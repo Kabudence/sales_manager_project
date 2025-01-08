2cbcecbcb0f801bba7345b11bebfec9613a5d58d
Josue Hidalgo Bustamante <164217768+Kabudence@users.noreply.github.com>
2025-01-08 01:07:54 -0500

    feat:added new database hidata

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:gitano200J%40%40J%40%40@localhost:3306/login_project'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:gitano200J%40%40J%40%40@localhost:3306/hidata'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'supersecretkey'
     JWT_SECRET_KEY = 'supersecretkey'
