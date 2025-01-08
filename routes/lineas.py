9fccede99dff2d15ce9232668505163c72af1366
Josue Hidalgo Bustamante <164217768+Kabudence@users.noreply.github.com>
2025-01-07 20:08:37 -0500

    feat:added new structure because of change in business logic

--- routes/lineas.py
+++ routes/lineas.py
@@ -10,19 +10,19 @@ linea_schema = LineaSchema()
 lineas_schema = LineaSchema(many=True)
 
 @linea_bp.route('/', methods=['GET'])
-@jwt_required()
+# @jwt_required()
 def get_all_lineas():
     lineas = Linea.query.all()
     return jsonify(lineas_schema.dump(lineas)), 200
 
 @linea_bp.route('/<int:id>', methods=['GET'])
-@jwt_required()
+# @jwt_required()
 def get_linea(id):
     linea = Linea.query.get_or_404(id)
     return jsonify(linea_schema.dump(linea)), 200
 
 @linea_bp.route('/', methods=['POST'])
-@jwt_required()
+# @jwt_required()
 def create_linea():
     data = request.get_json()
     errors = linea_schema.validate(data)
@@ -34,7 +34,7 @@ def create_linea():
     return jsonify(linea_schema.dump(linea)), 201
 
 @linea_bp.route('/<int:id>', methods=['PUT'])
-@jwt_required()
+# @jwt_required()
 def update_linea(id):
     linea = Linea.query.get_or_404(id)
     data = request.get_json()
@@ -47,7 +47,7 @@ def update_linea(id):
     return jsonify(linea_schema.dump(linea)), 200
 
 @linea_bp.route('/<int:id>', methods=['DELETE'])
-@jwt_required()
+# @jwt_required()
 def delete_linea(id):
     linea = Linea.query.get_or_404(id)
 
@@ -58,3 +58,9 @@ def delete_linea(id):
     db.session.delete(linea)
     db.session.commit()
     return jsonify({"message": "Linea eliminada exitosamente"}), 200
+
+@linea_bp.route('/api/lineas/<int:idlinea>/productos', methods=['GET'])
+def get_productos_por_linea(idlinea):
+    linea = Linea.query.get_or_404(idlinea)
+    productos = linea.productos.all()  # Obtiene los productos relacionados
+    return jsonify([producto.nomproducto for producto in productos])
