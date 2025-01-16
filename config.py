e6e444abd48b0830a9e449708124e83edc6c37dc
Josue Hidalgo Bustamante <164217768+Kabudence@users.noreply.github.com>
2025-01-16 10:57:02 -0500

    feat:added new security configuration

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
+from datetime import timedelta
+
+
 class Config:
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:gitano200J%40%40J%40%40@localhost:3306/hidata'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'supersecretkey'
     JWT_SECRET_KEY = 'supersecretkey'
-    JWT_ACCESS_TOKEN_EXPIRES = 3600
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=15)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=7)
